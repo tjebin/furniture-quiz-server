743a9224c787c5dd9b260a50c791ea89cd4589e2
tjebin-github <shova_khan@yahoo.com>
2022-09-08 07:52:59 -0400

    first commit

--- server.js
+++ server.js
@@ -32,4 +32,15 @@ require("./controllers/quizzes-controller")(app)
 require("./controllers/question-controller")(app)
 require('./controllers/quiz-attempts-controller')(app)
 
-app.listen(process.env.PORT || 2000)
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
+const PORT = process.env.PORT || 2000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
