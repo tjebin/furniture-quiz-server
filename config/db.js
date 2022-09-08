ff4699fb034e1c367eaa950097aa54797c3dd26c
tjebin-github <shova_khan@yahoo.com>
2022-09-08 08:17:55 -0400

    first commit

--- config/db.js
+++ config/db.js
@@ -1,21 +1,17 @@
-require('dotenv').config()
-
-
 const mongoose = require('mongoose');
 const config = require('config');
-// const db = config.get('mongoURI');
-
-const db = `${process.env.REACT_APP_MONGO_URI}`;
+const db = config.get('mongoURI');
 const connectDB = async () => {
     try {
-        await mongoose.createConnection(db, {
-            useNewUrlParser: true
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false
         });
-        console.log("MongoDB connected ... !!");
+        console.log('MongoDB/ Connected..');
     } catch (err) {
-        console.log(err.message);
-        ProcessingInstruction.exit(1);
+        console.error(err.message);
+        process.exit(1);
     }
 }
-
 module.exports = connectDB;
